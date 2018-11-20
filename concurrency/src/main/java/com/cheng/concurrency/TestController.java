3f5d9f19f96ecc07c5cee701516f9bfb9491dc8e
cheng <792702352@qq.com>
2018-11-21 00:23:25 +0800

    :sparkles: 并发模拟

---
+++ concurrency/src/main/java/com/cheng/concurrency/TestController.java
@@ -0,0 +1,21 @@
+package com.cheng.concurrency;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author cheng
+ *         2018/11/20 23:32
+ */
+@Controller
+@Slf4j
+public class TestController {
+
+    @RequestMapping("/test")
+    @ResponseBody
+    public String test() {
+        return "test";
+    }
+}
