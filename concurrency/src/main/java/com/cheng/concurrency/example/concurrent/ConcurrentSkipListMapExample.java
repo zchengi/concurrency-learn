c11fe51d2331a2227e3e70a8b2de957f89959f88
cheng <792702352@qq.com>
2018-11-22 19:26:00 +0800

    :sparkles: 线程安全 - 并发容器

---
+++ concurrency/src/main/java/com/cheng/concurrency/example/concurrent/ConcurrentSkipListMapExample.java
@@ -0,0 +1,59 @@
+package com.cheng.concurrency.example.concurrent;
+
+import com.cheng.concurrency.annotations.ThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Map;
+import java.util.concurrent.*;
+
+/**
+ * ConcurrentSkipListMap
+ *
+ * @author cheng
+ *         2018/11/22 19:19
+ */
+@Slf4j
+@ThreadSafe
+public class ConcurrentSkipListMapExample {
+    
+    /**
+     * 请求总数
+     */
+    private static int clientTotal = 5000;
+
+    /**
+     * 同时并发执行的线程数
+     */
+    private static int threadTotal = 200;
+
+    private static Map<Integer, Integer> map = new ConcurrentSkipListMap<>();
+
+    public static void main(String[] args) throws Exception {
+
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+
+        for (int i = 0; i < clientTotal; i++) {
+            final int count = i;
+            executorService.execute(() -> {
+                try {
+                    semaphore.acquire();
+                    update(count);
+                    semaphore.release();
+                } catch (Exception e) {
+                    log.error("Exception", e);
+                }
+                countDownLatch.countDown();
+            });
+        }
+
+        countDownLatch.await();
+        executorService.shutdown();
+        log.info("size: {}", map.size());
+    }
+
+    private static void update(int i) {
+        map.put(i, i);
+    }
+}
